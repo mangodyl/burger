501c3fdc74c0bbc57cf1591bb2ef97e178a62af5
mango dyl <dylanbonds@hotmail.com>
2019-03-10 22:11:12 -0500

    schema updated

--- db/schema.sql
+++ db/schema.sql
@@ -1,10 +1,10 @@
 CREATE DATABASE burgers_db;
 USE burgers_db;
 
-CREATE TABLE burger
-(
+CREATE TABLE burger (
     id int NOT NULL AUTO_INCREMENT,
     burger_name varchar(255) NOT NULL,
     devoured boolean DEFAULT false,
+    createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (id)
 );
